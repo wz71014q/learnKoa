03c17fef4d16cb4e2b12441efbc66f9a97d1c6ff
wz2131q <zhiqiang2726@163.com>
2020-05-15 13:51:30 +0800

    refactor: mysql pool

--- entries/mysql.js
+++ entries/mysql.js
@@ -1,15 +1,26 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
   password: 'wz2131q',
   database: 'database',
 });
-connection.connect();
+// pool.connect();
 
-connection.query('SELECT * FROM my_table',  (error, results, fields) => {
-  if (error) throw error
-  console.log(results)
-});
-connection.end();
+// pool.query('SELECT * FROM my_table',  (error, results, fields) => {
+//   if (error) throw error
+//   console.log(results)
+// });
+// pool.end();
+pool.getConnection(function(err, connection) {
+
+  connection.query('SELECT * FROM my_table',  (error, results, fields) => {
+
+    // 结束会话
+    connection.release();
+    // 如果有错误就抛出
+    if (error) throw error;
+      console.log(results)
+  })
+})
