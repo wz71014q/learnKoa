9a7715c6836cacfaff71b1ff4c3d037a4d9ea59b
wz2131q <zhiqiang2726@163.com>
2020-06-05 18:09:17 +0800

    css prefix

--- utils/resolveCSS.js
+++ utils/resolveCSS.js
@@ -10,12 +10,22 @@ function resolvePath(file) {
 
 async function resolveCss() {
   const myCss = await resolveFile(resolvePath("flex.css"))
-  const cssResult = await postcss([autoprefixer()]).process(myCss, {
-      from: resolvePath("flex.css"),
-      to: resolvePath("flex_result.css")
+  const cssResult = await postcss([
+    autoprefixer({
+      overrideBrowserslist: [
+        "last 2 versions",
+        "> 1%",
+        "iOS 7",
+        "last 3 iOS versions",
+        "not ie <= 8",
+        "Android >= 4.0"
+      ]
     })
+  ]).process(myCss, {
+    from: resolvePath("flex.css"),
+    to: resolvePath("flex_result.css")
+  })
   fs.writeFile(resolvePath("flex_result.css"), cssResult.css, (err, data) => {})
   if (cssResult.map) fs.writeFile(resolvePath("flex.css.map"), cssResult.map, (err, data) => {})
-  console.log(cssResult.css)
 }
 resolveCss()
